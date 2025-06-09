2ac98f97afe7e54517821ea4e25e026660857261
nooralu <nooralu@foxmail.com>
2025-06-09 13:12:55 +0800

    Part One: A Basic Parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,54 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+mod utils;
+
+#[derive(Debug, PartialEq)]
+pub struct Number(pub i32);
+
+impl Number {
+    pub fn new(s: &str) -> (&str, Self) {
+        let (s, number) = utils::extract_digits(s);
+        (s, Self(number.parse().unwrap()))
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+impl Op {
+    pub fn new(s: &str) -> (&str, Self) {
+        let (s, op) = utils::extract_op(s);
+
+        let op = match op {
+            "+" => Self::Add,
+            "-" => Self::Sub,
+            "*" => Self::Mul,
+            "/" => Self::Div,
+            _ => panic!("bad operator"),
+        };
+
+        (s, op)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Expr {
+    pub lhs: Number,
+    pub rhs: Number,
+    pub op: Op,
+}
+
+impl Expr {
+    pub fn new(s: &str) -> Self {
+        let (s, lhs) = Number::new(s);
+        let (s, op) = Op::new(s);
+        let (_s, rhs) = Number::new(s);
+
+        Self { lhs, rhs, op }
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +56,39 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn parse_number() {
+        assert_eq!(Number::new("123"), ("", Number(123)));
+    }
+
+    #[test]
+    fn parse_add_op() {
+        assert_eq!(Op::new("+"), ("", Op::Add));
+    }
+
+    #[test]
+    fn parse_sub_op() {
+        assert_eq!(Op::new("-"), ("", Op::Sub));
+    }
+
+    #[test]
+    fn parse_mul_op() {
+        assert_eq!(Op::new("*"), ("", Op::Mul));
+    }
+
+    #[test]
+    fn parse_div_op() {
+        assert_eq!(Op::new("/"), ("", Op::Div));
+    }
+
+    #[test]
+    fn parse_one_plus_two() {
+        assert_eq!(
+            Expr::new("1+2"),
+            Expr {
+                lhs: Number(1),
+                rhs: Number(2),
+                op: Op::Add,
+            },
+        );
     }
 }
