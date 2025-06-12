9ef71dc00e1a1c4b0d5d204e0d4728e9fade04e6
nooralu <nooralu@foxmail.com>
2025-06-12 13:59:37 +0800

    Part Seven: A REPL

---
+++ crates/eldiro/src/lib.rs
@@ -0,0 +1,30 @@
+pub mod binding_def;
+pub mod expr;
+pub mod stmt;
+
+mod env;
+mod utils;
+mod val;
+
+pub use env::Env;
+pub use val::Val;
+
+#[derive(Debug)]
+pub struct Parse(stmt::Stmt);
+
+impl Parse {
+    pub fn eval(&self, env: &mut Env) -> Result<Val, String> {
+      self.0.eval(env)
+    }
+}
+
+pub fn parse(s: &str) -> Result<Parse, String> {
+    let (s, stmt) = stmt::Stmt::new(s)?;
+
+    if s.is_empty() {
+        Ok(Parse(stmt))
+    } else {
+        dbg!(s);
+        Err("input was not consumed fully by parser".to_string())
+    }
+}
--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod binding_def;
-pub mod expr;
-pub mod stmt;
-
-mod env;
-mod utils;
-mod val;
